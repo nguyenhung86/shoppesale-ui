33a0af99e7cc5a798aa22b409236a017c3533fb9
nguyenhung86 <160987418+nguyenhung86@users.noreply.github.com>
2026-07-17 08:11:40 +0700

    Add files via upload

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 const CONFIG = {
   API_URL: "https://script.google.com/macros/s/AKfycbyL4WR2KTi-K41khmS7DBtoaDoNtRDsvF6GV1YTFDme8ynMAXdu2JgpPpYQalDi5a3VqQ/exec",
-  GOOGLE_CLIENT_ID: "YOUR_GOOGLE_CLIENT_ID_HERE" // Thay thế bằng Google Client ID thật của bạn
+  GOOGLE_CLIENT_ID: "452441495189-c2mkavu3bj33oqfemthcls5m28mrei09.apps.googleusercontent.com"
 };
