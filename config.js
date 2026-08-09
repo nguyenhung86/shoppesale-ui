dbf7d79f82b5c2796571b24477f3dde8e388195b
nguyenhung86 <160987418+nguyenhung86@users.noreply.github.com>
2026-08-09 10:01:54 +0700

    Add APPS_SCRIPT_URL fallback to config.js

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 const CONFIG = {
   API_URL: "https://api-vps.hoantienonline.io.vn/api/web",
+  APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbyL4WR2KTi-K41khmS7DBtoaDoNtRDsvF6GV1YTFDme8ynMAXdu2JgpPpYQalDi5a3VqQ/exec",
   GOOGLE_CLIENT_ID: "452441495189-c2mkavu3bj33oqfemthcls5m28mrei09.apps.googleusercontent.com"
 };
